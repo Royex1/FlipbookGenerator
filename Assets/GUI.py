d8d99725acd06e069bd347e589358bfe39a4fb55
Royex1 <52969185+Royex1@users.noreply.github.com>
2026-09-12 01:38:23 +0300

    1.0 Update
    - Folder Selection
    - Exporting Location + Export Name
    - Dynamic GUI
    - Ability to Export as JPG and PNG

--- Assets/GUI.py
+++ Assets/GUI.py
@@ -8,22 +8,61 @@ import Generate_Flipbook as gf
 class MyWindow(QMainWindow):
     def __init__(self):
         super(MyWindow, self).__init__()
-        self.setGeometry(int(800), int(400), 230, 170)
+        self.setGeometry(800, 400, 350, 230)
         self.setWindowTitle("Flipbook Generator")
         self.extracted_vars = []
         self.initUI()
 
     def initUI(self):
 
+        # Source Folder -------------------------------------------------------------
+        self.SourceLabel = QtWidgets.QLabel(self)
+        self.SourceLabel.setText("Source Folder:")
+        self.SourceLabel.move(20,20)
+        self.SourceLabel.adjustSize()
+
+        self.SourceTextBox = QtWidgets.QLineEdit(self)
+        self.SourceTextBox.setPlaceholderText("Put Folder Location")
+        self.SourceTextBox.setText("./Sprites")        
+        self.SourceTextBox.move(120,16)
+        self.SourceTextBox.setFixedSize(200,25)
+
+        # Exported Folder -------------------------------------------------------------
+        self.ExportedLabel = QtWidgets.QLabel(self)
+        self.ExportedLabel.setText("Exported Image:")
+        self.ExportedLabel.move(20,50)
+        self.ExportedLabel.adjustSize()
+
+        self.ExportedTextBox = QtWidgets.QLineEdit(self)
+        self.ExportedTextBox.setPlaceholderText("Image Location And Name")
+        self.ExportedTextBox.setText("./Flipbook")        
+        self.ExportedTextBox.move(120,46)
+        self.ExportedTextBox.setFixedSize(200,25)
+
+
+        # Export Alpha -------------------------------------------------------------
+        self.AlphaLabel = QtWidgets.QLabel(self)
+        self.AlphaLabel.setText("Export Alpha")
+        self.AlphaLabel.move(20,90)
+        self.AlphaLabel.adjustSize()
+
+        self.AlphaBox = QtWidgets.QCheckBox(self)
+        self.AlphaBox.setCheckState(2)
+        self.AlphaBox.move(110,90)
+        self.AlphaBox.adjustSize()
+
+
+        
         # Dynamic Rows -------------------------------------------------------------
         self.DynamicRowLabel = QtWidgets.QLabel(self)
         self.DynamicRowLabel.setText("Dynamic Rows")
-        self.DynamicRowLabel.move(20,20)
+        self.DynamicRowLabel.move(20,120)
         self.DynamicRowLabel.adjustSize()
 
         self.DynamicRowBox = QtWidgets.QCheckBox(self)
-        self.DynamicRowBox.setCheckState(0)
-        self.DynamicRowBox.move(110,20)
+        self.DynamicRowBox.setCheckState(2)
+        self.DynamicRowBox.stateChanged.connect(self.UpdateGUI)
+        self.DynamicRowBox.move(110,120)
         self.DynamicRowBox.adjustSize()
 
 
@@ -31,13 +70,13 @@ class MyWindow(QMainWindow):
         # Columns X ---------------------------------------------------------------------
         self.ColumnLabel = QtWidgets.QLabel(self)
         self.ColumnLabel.setText("Number Of Columns (X)")
-        self.ColumnLabel.move(20,55)
+        self.ColumnLabel.move(20,155)
         self.ColumnLabel.adjustSize()
 
         self.ColumnBox = QtWidgets.QSpinBox(self)
-        self.ColumnBox.setRange(0,100)
+        self.ColumnBox.setRange(0,1000)
         self.ColumnBox.setValue(3)
-        self.ColumnBox.move(170,54)
+        self.ColumnBox.move(170,154)
         self.ColumnBox.adjustSize()
 
 
@@ -45,13 +84,13 @@ class MyWindow(QMainWindow):
         # Rows Y ---------------------------------------------------------------------
         self.RowLabel = QtWidgets.QLabel(self)
         self.RowLabel.setText("Number Of Rows (Y)")
-        self.RowLabel.move(20,90)
+        self.RowLabel.move(10000,190)
         self.RowLabel.adjustSize()
 
         self.RowBox = QtWidgets.QSpinBox(self)
-        self.RowBox.setRange(0,100)
+        self.RowBox.setRange(0,1000)
         self.RowBox.setValue(3)
-        self.RowBox.move(150,89)
+        self.RowBox.move(10000,189)
         self.RowBox.adjustSize()
 
 
@@ -60,7 +99,7 @@ class MyWindow(QMainWindow):
         self.generateButton = QtWidgets.QPushButton(self)
         self.generateButton.setText("Generate Flipbook")
         self.generateButton.clicked.connect(self.generateFlipbook)
-        self.generateButton.move(50,125)
+        self.generateButton.move(20,190)
         self.generateButton.adjustSize()
 
 
@@ -68,10 +107,25 @@ class MyWindow(QMainWindow):
     # Generate Images
 
     def generateFlipbook(self):
-        finalValues = [str(self.ColumnBox.value()),
-                       str(self.RowBox.value()),
-                       str(self.DynamicRowBox.isChecked())]
-        gf.generate_flipbook(finalValues)
+        
+        gf.generate_flipbook(self.SourceTextBox.text(), 
+                             self.ExportedTextBox.text(), 
+                             str(self.ColumnBox.value()), 
+                             str(self.RowBox.value()), 
+                             str(self.DynamicRowBox.isChecked()), 
+                             str(self.AlphaBox.isChecked()))
+
+    def UpdateGUI(self):
+        if self.DynamicRowBox.checkState() == 2:
+            self.RowLabel.move(10000,190)
+            self.RowBox.move(10000,189)
+            self.generateButton.move(20,190)
+            self.resize(350, 230)
+        else:
+            self.RowLabel.move(20,190)
+            self.RowBox.move(150,189)
+            self.generateButton.move(20,220)
+            self.resize(350, 260)
 
 
 
--- Assets/Generate_Flipbook.py
+++ Assets/Generate_Flipbook.py
@@ -2,48 +2,53 @@ import glob
 import os
 from PIL import Image
 
+def Alpha(AlphaValue):
+    if AlphaValue == "True":
+        return "RGBA", ".png", "#ff000000"
+    else:
+        return "RGB", ".jpg", "#000000FF"
 
-def generate_flipbook(Values):
+
+def generate_flipbook(SourceFolder,ExportLocation,Col,Rw,DynamicRows,ExportAlpha):
     # For Spread Logic
     tempX = 0
     tempY = 0
 
     # Get All Sprite Images
-    png_files = glob.glob(os.path.join("./SpriteSheets", '*.*'))
-
-    # Set Output Path
-    output_path = "./Flipbook.png"
+    imgs = glob.glob(os.path.join(str(SourceFolder), '*.*'))
 
 
-    Columns = int(Values[0])
-    Rows = int(Values[1])
+    Columns = int(Col)
+    Rows = int(Rw)
 
     # Check If Dynamic Rows is enabled
-    if Values[2] == "True":
+    if DynamicRows == "True":
 
         # Resets Rows because we no longer need the var above
         Rows = 0
 
         # Start Looking For The Correct Row Value
-        while len(png_files) > Rows * Columns:
+        while len(imgs) > Rows * Columns:
             Rows = Rows + 1
 
     # Get Sprite Size
-    Sprite_X, Sprite_Y = Image.open(png_files[0]).size
+    Sprite_X, Sprite_Y = Image.open(imgs[0]).size
 
     # Know the total size of our Flipbook then create an empty image
     Flipbook_Size = [Sprite_X * Columns, Sprite_Y * Rows]
-    Flipbook_Image = Image.new("RGBA", Flipbook_Size, "#ff000000")
+    Flipbook_Image = Image.new(Alpha(ExportAlpha)[0], Flipbook_Size, Alpha(ExportAlpha)[2])
 
-    for png in png_files:
+    for img in imgs:
         
         # Open the image
-        Sprite = Image.open(png)
+        Sprite = Image.open(img)
 
         # paste our sprite to the flipbook image (we multiply with tempX/Y because these are the locations of our pasted sprite)
         # Note: even alpha paste over RGB so if there's alpha value, it will replace RGB value into alpha
-        Flipbook_Image.paste(Sprite, (Sprite_X * tempX, Sprite_Y * tempY))
-
+        if ExportAlpha == "True":
+            Flipbook_Image.paste(Sprite, (Sprite_X * tempX, Sprite_Y * tempY))
+        else:
+            Flipbook_Image.paste(Sprite, (Sprite_X * tempX, Sprite_Y * tempY), Sprite)
         # Spread Logic
         if tempX < Columns - 1:
 
@@ -53,4 +58,4 @@ def generate_flipbook(Values):
             tempY = tempY + 1
         
     # Save our image
-    Flipbook_Image.save(output_path)
+    Flipbook_Image.save(ExportLocation + Alpha(ExportAlpha)[1])
